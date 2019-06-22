b6ee1890d28b2ae7fe53b942017b272ffedd63db
Shwe N Aye <shweaye810@gmail.com>
2019-06-22 17:27:43 -0500

    enable security
    - permit /api/oauth/* without authorization
    - enable CORS to * origins

---
+++ src/main/java/com/ss/storyboxzuul/WebConfiguration.java
@@ -0,0 +1,32 @@
+package com.ss.storyboxzuul;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration extends ResourceServerConfigurerAdapter implements WebMvcConfigurer {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests().antMatchers("/api/oauth/**").permitAll().antMatchers("/**")
+		.authenticated();
+
+	}
+	
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+}
